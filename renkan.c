14200a4e916a3ff4483c18a78a4ef6d8af38715d
Akagi <kaya3728@gmail.com>
2017-01-11 19:31:26 +0900

    succsess parsing

--- renkan.c
+++ renkan.c
@@ -4,24 +4,37 @@
 #include <stdlib.h>
 
 //macros
-#define CSV_ROW 1000
+#define CSV_ROW 10000
 
 int main(void){
-
+    int ret;
     FILE *fp;
-    char *fname = "./20161006data/test.csv"
-    double value[CSV_ROW] ;
-    char goods[CSV_ROW],hCity[CSV_ROW],zCity[CSV_ROW],hCd[CSV_ROW],zCd[CSV_ROW],hInd[CSV_ROW],zInd[CSV_ROW];
+    char *fname = "../../20161006data/test.csv";
+    float value[CSV_ROW], goods[CSV_ROW],hCity[CSV_ROW],zCity[CSV_ROW],hCd[CSV_ROW],zCd[CSV_ROW],hInd[CSV_ROW],zInd[CSV_ROW];
+    char header[8][20];
 
     fp = fopen(fname, "r");
     if(fp == NULL ){
         printf("Error:can't open file.\n");
         return -1;
     }
+
+    fscanf(fp,"%[^,],%[^,],%[^,],%[^,],%[^,],%[^,],%[^,],%s",header[0],header[1],header[2],header[3],header[4],header[5],header[6],header[7]);
+    for(int i = 0; i < 8 ; i++){
+        printf("%s\n",header[i]);
+    }
     
-    while((ret = fscanf(fp,"%[^,],%f,%f",))
+    int i = 0; 
+        
+    while((ret = fscanf(fp,"%f,%f,%f,%f,%f,%f,%f,%f",&hCd[i],&zCd[i],&hInd[i],&zInd[i],&goods[i],&hCity[i],&zCity[i],&value[i])) != EOF){
+            printf("%f \n",hCd[i]);
+            i ++;
+    }
 
-    fscanf()
+    printf("Number Of data is ");
 
+    fclose(fp);
+
+    return 0;
 
 }
